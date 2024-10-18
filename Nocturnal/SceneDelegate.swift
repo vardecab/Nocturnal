93d13d0c578a71321f7b926893416e4d80d083fd
vardecab <6877391+vardecab@users.noreply.github.com>
2024-10-18 11:19:34 +0000

    Initial Commit

---
+++ Nocturnal/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Nocturnal
+//
+//  Created by Kuba R on 18/10/2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
